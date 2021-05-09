0bb9b84e6b44ccd079629b066871651fea689e84
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-05-09 12:16:40 +0800

    Add selector placeholder functions to Map

---
+++ game/src/map/Map.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const TILE_SIZE = 64.0
+enum tile_content {ROAD, EMPTY_PLOT, TURRET, BASE, INVALID}
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			var x = event.position.x / TILE_SIZE
+			var y = event.position.y / TILE_SIZE
+			$Selector.select(x, y)
+
+func get_contents_at(x: int, y : int) -> void:
+	# Check for turrets
+	# Check for path
+	return
+
+func is_valid_turret_drop(x: int, y: int) -> bool:
+	# for now all values return true
+	return true
+
+func build_turret_at(x: int, y: int) -> void:
+	# var turret = preload(...).instance()
+	# add_child(turret)
+	# turret.position(Vector2(x,y) * TILE_SIZE)
+	return
