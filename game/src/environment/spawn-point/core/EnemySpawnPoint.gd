2d7db19c4963bc97ff2ac945f9ead215bc580b02
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-05-19 15:29:43 +0800

    Reorganise src and support asset changes.

--- game/src/environment/spawn-point/core/EnemySpawnPoint.gd
+++ game/src/environment/spawn-point/core/EnemySpawnPoint.gd
@@ -2,7 +2,7 @@ extends Area2D
 class_name EnemySpawnPoint
 
 # constants
-export var spawn_rate := 0.04
+export var spawn_rate := 1.0
 
 # hidden state variables
 var _last_spawn := 0.0
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 	_last_spawn += delta
 
 func spawn() -> void:
-	var enemy = preload("res://src/enemy/Enemy.tscn").instance()
+	var enemy = preload("res://src/enemy/core/Enemy.tscn").instance()
 	get_parent().add_child(enemy)
 	enemy.global_position = self.global_position
 	enemy.set_target(get_parent().get_node("Home"))
---
+++ game/src/projectile/core/Projectile.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+class_name Projectile
+
+export var speed := 200.0
+export var damage := 20.0
+
+# enemy state variables
+var origin : Vector2
+var target : Node2D
+
+func _ready() -> void:
+	self.global_position = origin
+	if target == null or weakref(target).get_ref() == null:
+		_stop()
+	else:
+		look_at(target.position)
+
+func _physics_process(delta: float) -> void:
+	_move_toward_target(delta)
+
+func fire(src: Vector2, dest: Node2D) -> void:
+	origin = src
+	target = dest
+
+func _move_toward_target(delta: float) -> void:
+	if target == null or weakref(target).get_ref() == null:
+		_stop()
+		return
+		
+	var direction := target.position
+	direction -= position
+	var velocity := direction.normalized() * speed
+	self.position += velocity * delta
+	self.look_at(target.position)
+
+func _stop() -> void:
+	queue_free()
+
+func inflict_damage(body : Node2D) -> void:
+	if body.health > 0.0:
+		body.health -= damage
+		self.queue_free()
+	elif body == target:
+		self.queue_free()
