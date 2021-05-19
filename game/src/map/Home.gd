fef1a2700160ce43966e1210382641b971ae677e
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-05-19 11:07:03 +0800

    Remove kenny resource paths

--- game/src/map/Home.gd
+++ game/src/map/Home.gd
@@ -4,6 +4,9 @@ class_name Home
 signal take_damage(damage, new_health)
 onready var health := 100.0
 
+func _ready() -> void:
+	$MarginContainer/HBoxContainer/HealthVal.set_text(str(health))
+
 # Watches for enemy weapon hits
 func _on_Base_body_entered(body: Node) -> void:
 	if body is EnemyWeapon:
