d9a043f7bb49411ebdad6539267b628b877b571c
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-05-24 14:39:08 +0800

    Copy over map and enemy folders.
    from bhcs/enemy-upgrade-assets

--- game/src/enemy/core/Enemy.gd
+++ game/src/enemy/core/Enemy.gd
@@ -3,87 +3,129 @@ class_name Enemy
 
 # % of random movement to avoid the 'queue' clustering effect'
 const RAND_WALK_EFFECT := 50
+const RANDOM_WALK_DIST := 128.0 * 0.4
 
-# enemy body specific constants
-export var speed := 400.0
-export var _corpse_timer := 5.0
-export var _rate_of_fire := 2.0
-
-# enemy state variables
-var health := 100.0
+# basic enemy variables
+var _speed := 200.0
+var _health := 100.0
 var _target : Home
-var _last_fire := 0.0
-var alive = true
+
+# current enemy behaviour
+var action
+enum ACTION { IDLE, MOVE, ATTACK, TAKE_DAMAGE, DIE }
+
+# durations for freezing in a certain behaviour
+var damage_taken_time := 0.5
+var death_rattle_time := 2.0
+var attack_time := 2.0
 
 var path := PoolVector2Array()
 var _is_reached := false
 
-signal enemy_dead
+onready var sprite : AnimatedSprite = $Sprite
+onready var damage_taken_timer : Timer = $DamageTakenTimer
+onready var attack_timer : Timer = $AttackTimer
+onready var collision_timer : Timer = $CollisionTimer
 
 func _ready() -> void:
+	action = self.ACTION.IDLE
+	self.sprite.play("idle")
 	self.add_to_group("enemies")
+	collision_timer.set_paused(true)
 
 func _physics_process(delta: float) -> void:
-	if _corpse_timer <= 0.0:
-		queue_free()
+	if action == ACTION.DIE:
 		return
-		
-	if health <= 0.0:
-		kill()
-		_corpse_timer -= delta
+	elif !damage_taken_timer.is_stopped():
 		return
-		
-	if not _is_reached:
-		move_along_path(delta * speed)
-	elif _last_fire > 1.0 / _rate_of_fire:
-		_last_fire = 0.0
-		fire()
+	elif not _is_reached:
+		if collision_timer.is_paused():
+			if not _target: 
+				_set_new_random_walk()
+			_move_along_path(delta * _speed)
+		elif collision_timer.is_stopped() or path.size() == 0:
+			collision_timer.set_paused(true)
+			if _target: 
+				set_target(_target)
+		else:
+			_move_along_path(delta * _speed)
 	else:
-		_last_fire += delta
+		attack()
+
+func _move_along_path(distance: float) -> void:
+	action = self.ACTION.MOVE
+	if sprite.get_animation() != "move":
+		sprite.play("move")
 	
-func move_along_path(distance : float) -> void:
 	var start_point := position
-	for _i in range(path.size()):
-		var distance_to_next := start_point.distance_to(path[0])
-		if distance <= distance_to_next and distance >= 0.0:
-			var next_position := start_point.linear_interpolate(
-				path[0], distance / distance_to_next)
-			var randVect := Vector2(randf()-0.5, randf()-0.5) / 100
-			randVect = Vector2(1, 1) + randVect * RAND_WALK_EFFECT
-			move_and_collide((next_position - position) * randVect)
-			break
-		elif distance < 0.0:
-			_is_reached = true
-			move_and_collide(path[0] - position)
-			break
+	
+	if path.size() == 0:
+		return
+	var distance_to_next := start_point.distance_to(path[0])
+	if  distance_to_next == 0:
+		path.remove(0)
+		return
+	if distance > distance_to_next or distance < 0.0:
 		distance -= distance_to_next
 		start_point = path[0]
 		path.remove(0)
+		_move_along_path(distance)
+		return
 	
-func set_path(value : PoolVector2Array) -> void:
-	path = value
-	_is_reached = false
+	var next_position := start_point.linear_interpolate(
+		path[0], distance / distance_to_next)
+	var randVect := Vector2(randf()-0.5, randf()-0.5) / 100
+	randVect = Vector2(1, 1) + randVect * RAND_WALK_EFFECT
+	var move_vector := (next_position - position) * randVect
+	var collision = move_and_collide(move_vector)
+	if collision:
+		_set_new_random_walk()
+		collision_timer.set_paused(false)
+		collision_timer.start()
+
+
+func _set_new_random_walk() -> void:
+	var new_target := Vector2(randf() - 0.5, randf() - 0.5).normalized() * RANDOM_WALK_DIST
+	path = _set_target(new_target)
 
 # fire weapon at home
-func fire() -> void:
+func attack() -> void:
+	if !attack_timer.is_stopped():
+		return
+	action = self.ACTION.ATTACK
+	sprite.play("attack")
+	attack_timer.start()
 	var weapon = preload("res://src/projectile/enemy-weapon/EnemyWeapon.tscn").instance()
 	weapon.fire(global_position, _target)
 	get_parent().add_child(weapon)
 
+# take damage
+func take_damage(damage_taken: float) -> void:
+	_health -= damage_taken
+	if _health <= 0.0:
+		_kill()
+		return
+	
+	damage_taken_timer.start(damage_taken_time)
+	if action != ACTION.TAKE_DAMAGE:
+		sprite.play("take_damage")
+		action = ACTION.TAKE_DAMAGE
 
-# to be run when health reaches 0
-func kill() -> void:
-	health = 0.0
-	$Sprite.set_animation("dead")
-	emit_signal("enemy_dead")
+func _kill() -> void:
+	action = ACTION.DIE
+	damage_taken_timer.start(death_rattle_time)
+	sprite.play("die")
+	# warning-ignore:return_value_discarded
+	damage_taken_timer.connect("timeout", self, "queue_free")
 
-# refresh home location
-# currently not in use, safe to delete
-func set_target(target : Home):
+func set_target(target : Home) -> void:
 	_target = target
+	path = _set_target(target.global_position - self.global_position)
+
+func _set_target(relative_position_target : Vector2) -> PoolVector2Array:
 	var nav : Navigation2D
 	nav = get_parent().get_node("Navigator")
-	path = nav.get_simple_path(global_position, target.global_position, true)
+	return nav.get_simple_path(global_position, global_position + relative_position_target, true)
 
 # registers when home has been encountered
 func _on_Range_area_entered(area: Area2D) -> void:
@@ -92,4 +134,5 @@ func _on_Range_area_entered(area: Area2D) -> void:
 
 func _on_body_entering_vitals(body: Node) -> void:
 	if body is Projectile:
+		# warning-ignore:unsafe_method_access
 		body.inflict_damage(self)
