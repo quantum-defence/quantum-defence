b3302f6f4fa5672ed8a300996e91d05c5f1343e4
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-05-27 00:37:00 +0800

    Complete TileSelector functionality.

--- game/src/environment/towers/core/Tower.gd
+++ game/src/environment/towers/core/Tower.gd
@@ -1,5 +1,17 @@
 extends Node2D
 class_name Tower
+enum TYPES { 
+	FASTEST = 0,
+	REPEATER = 1, 
+	SHOTGUN = 2, 
+	ULTIMATE = 3,
+}
+const RESOURCE = [
+	'res://src/environment/towers/FastestTower.tscn',
+	'res://src/environment/towers/RepeaterTower.tscn',
+	'res://src/environment/towers/ShotgunTower.tscn',
+	'res://src/environment/towers/UltimateTower.tscn'
+]
 
 # var firing_interval := 0.2
 #External properties
@@ -7,10 +19,31 @@ class_name Tower
 onready var _target : Enemy = null
 onready var _enemiesInRange := []
 onready var _timer : Timer = $Timer
+onready var _attributes := [0, 0, 0, 0] # assuming 4 attributes
 
 func _ready() -> void:
 	#_timer.set_wait_time(firing_interval)
+# warning-ignore:return_value_discarded
 	_timer.connect("timeout", self, "_fire")
+
+func build_at(position: Vector2) -> void:
+	self.position = position
+	# show animation
+
+func dismantle() -> Array:
+	# show animation
+	var attr = self._attributes
+	queue_free()
+	return attr
+
+func upgrade_with(item_type: int) -> bool:
+	# Item.TYPES should be an enum that handles this
+	var current_level = self._attributes[item_type]
+	if current_level < 5:
+		self._attributes[item_type] = current_level + 1
+		return true
+	return false
+	
 
 func _fire() -> void:
 	var weapon = preload("res://src/projectile/core/Projectile.tscn").instance()
@@ -33,6 +66,7 @@ func _choose_enemy() -> Enemy:
 			chosen_enemy = enemy
 	return chosen_enemy
 
+# warning-ignore:unused_argument
 func _process(delta: float) -> void:
 	_target = _choose_enemy()
 	if (_target != null and _timer.is_stopped()):
--- game/src/ui/gameplay/tile-selector/TileSelector.gd
+++ game/src/ui/gameplay/tile-selector/TileSelector.gd
@@ -1,15 +1,55 @@
 extends Area2D
 class_name TileSelector
 
-const TILE_SIZE := 128.0
+# DUPLICATE CODE: must also modify at map
+const TILE_SIZE = 128.0
+enum TILE_CONTENTS { PATH, EMPTY, TOWER, HOME, INVALID = -1 }
+enum ACTION { BUILDING, DISMANTLING, DROPPING, INSPECTING }
 
 var _x : int
 var _y : int
+var _action : int
+var _type : int
+var _valid_tile : bool
+
+onready var arena : Arena = get_parent()
 
 func select(x : int, y : int) -> void:
 	_x = x
 	_y = y
 	position = Vector2(x,y) * TILE_SIZE
+	print(_is_valid_tile())
 
-func get_contents_at() -> void:
-	get_parent().get_contents_at(_x, _y)
+# if action == ACTION.BUILDING, type must be of Tower.TYPE
+# likewise if action == ACTION.DROPPING, type must be of Item.TYPE
+func set_action(action : int, type : int) -> void:
+	_action = action
+	_type = type
+
+func _process(delta: float) -> void:
+	self.modulate = Color(0, 1, 0) if _is_valid_tile() else Color(1, 0, 0)
+	
+func _is_valid_tile() -> bool:
+	var tile = arena.get_contents_at(_x, _y)
+	print(tile)
+	match (_action):
+		ACTION.DROPPING:
+			return tile == TILE_CONTENTS.EMPTY
+		ACTION.BUILDING:
+			return tile == TILE_CONTENTS.EMPTY
+		ACTION.DISMANTLING:
+			return tile == TILE_CONTENTS.TOWER
+		_: # INSPECTING
+			return true
+
+func take_action():
+	match (_action):
+		ACTION.DROPPING:
+			return arena.drop_items(_x, _y, _type)
+		ACTION.BUILDING:
+			return arena.build_tower(_x, _y, _type)
+		ACTION.DISMANTLING:
+			return arena.dismantle_tower(_x, _y)
+		_: # INSPECTING
+			# idk maybe show a pop up for towers to inspect?
+			return 0
