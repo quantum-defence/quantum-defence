7e10c57ffc4258959932db6f6e6cf3f6da68305c
Bluntsord <teechin1999@gmail.com>
2021-06-20 16:52:25 +0800

    Fixed droppable Items

--- game/src/items/core/DroppableItems.gd
+++ game/src/items/core/DroppableItems.gd
@@ -7,6 +7,7 @@ onready var build_UI = get_parent().get_node("UI").get_node("Control").get_node(
 onready var item = get_node("Item")
 
 func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx:int):
+	print("Before click")
 	if (event.is_pressed() and event.button_index == BUTTON_LEFT):
 		print("Comes here")
 		build_UI._pick_up_item(item)
