804c82aaf4199144016603473042222b878a176b
Tee Chin <teechin1999@gmail.com>
2021-06-27 12:28:51 +0800

    Minor update on audio player

--- game/src/ui/Menu/Menu.gd
+++ game/src/ui/Menu/Menu.gd
@@ -1,15 +1,16 @@
 extends Control
 
 const first_scene = preload("res://src/environment/map/Map.tscn")
-
+onready var background_music = self.get_node("AudioStreamPlayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var background_music = self.get_node("AudioStreamPlayer")
 	background_music.playing = true
 	pass # Replace with function body.
 
-
+func _process(delta):
+	if (background_music.playing == false):
+		background_music.playing = true
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://src/environment/map/Map.tscn")
