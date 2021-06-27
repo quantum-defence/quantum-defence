c6cd120be2b422f524327515007cde6f9d964b0e
Tee Chin <teechin1999@gmail.com>
2021-06-27 12:24:36 +0800

    Update on the Menu. Added buttons for it that links to the main map. Settings, gallery and credits still have no function

--- game/src/ui/Menu/Menu.gd
+++ game/src/ui/Menu/Menu.gd
@@ -1,9 +1,6 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const first_scene = preload("res://src/environment/map/Map.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,6 +10,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://src/environment/map/Map.tscn")
