cf649cc2832f3d49b917c7f3867197a9d896b127
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-06-30 01:26:13 +0800

    Manage signals within LevelMap and its components.

--- game/src/environment/LevelMap/core/LevelMap.gd
+++ game/src/environment/LevelMap/core/LevelMap.gd
@@ -2,17 +2,38 @@ extends Node2D
 
 class_name LevelMap
 
+signal on_prep_time_start(duration)
+signal on_spawn_cycle(cycle_number, duration)
+signal on_cooldown_cycle(cycle_number, duration)
+signal on_game_end(is_win, blue_health, red_health, cycle_number, enemy_spawn_count, enemy_kill_count)
+signal on_enemy_death
+signal on_last_cycle
+
 onready var blue_home : Home = $BlueHome
 onready var red_home : Home = $RedHome
 onready var tile_skeleton : TileMap = $TileMapSkeleton
 onready var initial_items : Array = ['h', 'x', 'ry']
 export var level_name : String = "core"
 
-#func _ready() -> void:
-	#level_name = "basic"
-	#$Navigator/NavPolyInstance.set_enabled(false)
-	#$Navigator/NavPolyInstance.queue_free()
-	#$Navigator/NavigationPolygonInstance.set_enabled(true)
+var prep_time := 6.0
+var spawn_rate := 0.8
+var spawn_on_time := 10.0
+var spawn_off_time := 20.0
+var max_spawn_cycle := 4
+
+var blue_health := 100
+var red_health := 100
+var enemy_spawn_count := 0
+var enemy_kill_count := 0
+var cycle_number := 0
+
+func _ready() -> void:
+	var spawn_point : EnemySpawnPoint = $EnemySpawnPoint
+	spawn_point.spawn_off_time = spawn_off_time
+	spawn_point.spawn_on_time = spawn_on_time
+	spawn_point.max_spawn_cycle = max_spawn_cycle
+	spawn_point.spawn_rate = spawn_rate
+	spawn_point.begin_prep_clock(prep_time)
 
 # Make an INBUILT script and do the following for any inherited LevelMap:
 # DO NOT edit the NavPolyInstance and instead make your own NavigationPolygonInstance to ensure no overwriting
@@ -21,3 +42,37 @@ export var level_name : String = "core"
 	# $Navigator/NavPolyInstance.set_enabled(false)
 	# $Navigator/NavPolyInstance.queue_free()
 	# $Navigator/NavigationPolygonInstance.set_enabled(true)
+
+func _on_prep_start(duration) -> void:
+	# print("on prep start " + str(duration))
+	emit_signal("on_prep_time_start", duration)
+
+func _on_home_damage(isRed, health) -> void:
+	# print("home damage" + " red " if isRed else " blue " + str(health))
+	if isRed:
+		red_health = health
+	else:
+		blue_health = health
+	if (blue_health <= 0 or red_health <= 0):
+		emit_signal("on_game_end", false, blue_health, red_health, cycle_number, enemy_spawn_count, enemy_kill_count)
+
+func _on_enemy_kia() -> void:
+	enemy_kill_count += 1
+	emit_signal("on_enemy_death")
+
+func _on_enemy_spawn() -> void:
+	enemy_spawn_count += 1
+
+func _on_last_enemy_dead() -> void:
+	if (blue_health > 0 and red_health > 0):
+		emit_signal("on_game_end", true, blue_health, red_health, cycle_number, enemy_spawn_count, enemy_kill_count)
+
+func _on_max_cycle_reached() -> void:
+	emit_signal("on_last_cycle")
+
+func _on_spawn_cycle_change(is_spawning, cycle_number, duration) -> void:
+	self.cycle_number = cycle_number
+	if is_spawning:
+		emit_signal("on_spawn_cycle", cycle_number, duration)
+	else:
+		emit_signal("on_cooldown_cycle", cycle_number, duration)
--- game/src/environment/spawn-point/core/EnemySpawnPoint.gd
+++ game/src/environment/spawn-point/core/EnemySpawnPoint.gd
@@ -7,37 +7,77 @@ onready var ENEMY_TYPES = {
 }
 
 # constants
-export var spawn_rate := 0.5
-export var on_off_interval := [4,16]
+var spawn_rate := 0.5
+var spawn_on_time := 10.0
+var spawn_off_time := 20.0
+var max_spawn_cycle := 4
+
+signal on_prep_start(duration)
+signal on_spawn_cycle_change(is_spawning, cycle_number, duration)
+signal on_max_cycle_reached
+signal on_enemy_kia
+signal on_enemy_spawn
+signal on_last_enemy_dead
 
 # hidden state variables
 var _last_spawn := 0.0
 var is_spawning : bool = true
+var prep_timer : Timer
 var cooldown_timer : Timer
 var spawn_timer : Timer
+var cycle_number : int = 0
+var living_enemy_count: int = 0
 
 func _ready() -> void:
+	set_process(false)
+
+func begin_prep_clock(prep_time: float) -> void:
+	prep_timer = Timer.new()
+	prep_timer.wait_time = prep_time
+	prep_timer.autostart = false
+	prep_timer.one_shot = true
+	add_child(prep_timer)
+	prep_timer.connect("timeout", self, "_begin_spawn_cycles")
+	prep_timer.start()
+	emit_signal("on_prep_start", prep_time)
+
+func _begin_spawn_cycles() -> void:
+	prep_timer.stop()
+	prep_timer.queue_free()
+	prep_timer = null
+	
 	cooldown_timer = Timer.new()
 	spawn_timer = Timer.new()
-	cooldown_timer.wait_time = on_off_interval[1]
-	spawn_timer.wait_time = on_off_interval[0]
+	cooldown_timer.wait_time = spawn_off_time
+	spawn_timer.wait_time = spawn_on_time
 	cooldown_timer.autostart = false
 	spawn_timer.autostart = false
 	cooldown_timer.one_shot = true
 	spawn_timer.one_shot = true
+	
 	add_child(cooldown_timer)
 	add_child(spawn_timer)
 	spawn_timer.start()
+	emit_signal("on_spawn_cycle_change", true, cycle_number, spawn_on_time)
+	set_process(true)
 
 func _process(delta: float) -> void:
 	if is_spawning:
 		if spawn_timer.is_stopped():
+			if max_spawn_cycle <= cycle_number:
+				emit_signal("on_max_cycle_reached")
+				set_process(false)
+				return
 			cooldown_timer.start()
+			print('fasfsf')
+			emit_signal("on_spawn_cycle_change", false, cycle_number, spawn_off_time)
 			is_spawning = false
 	else: 
 		if cooldown_timer.is_stopped():
+			cycle_number += 1
 			spawn_timer.start()
 			is_spawning = true
+			emit_signal("on_spawn_cycle_change", true, cycle_number, spawn_on_time)
 	if !is_spawning:
 		return
 	elif _last_spawn > 1.0 / spawn_rate:
@@ -46,9 +86,18 @@ func _process(delta: float) -> void:
 	_last_spawn += delta
 
 func spawn() -> void:
-	var enemy = ENEMY_TYPES.SABRE.instance()
+	living_enemy_count += 1
+	var enemy : Enemy = ENEMY_TYPES.SABRE.instance()
+	enemy.connect("kia", self, "register_kia")
 	get_parent().add_child_below_node(self, enemy)
 	enemy.set_up(
 		self.global_position, 
 		get_parent().get_node("RedHome"),
 		get_parent().get_node("BlueHome"))
+	emit_signal("on_enemy_spawn")
+
+func register_kia() -> void:
+	emit_signal("on_enemy_kia")
+	living_enemy_count -= 1
+	if max_spawn_cycle <= cycle_number:
+		emit_signal("on_last_enemy_dead")
