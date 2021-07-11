773b022b8c4e0e41aa0b4adf46d44033a73a1ee2
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-07-11 11:31:28 +0800

    Add ability to switch between levels
     * Level names and sources not hardcoded\
     * Easy to extend in the future with on hover pop ups

--- game/src/ui/Menu/Menu.gd
+++ game/src/ui/Menu/Menu.gd
@@ -2,11 +2,6 @@ extends Control
 
 onready var background_music = self.get_node("AudioStreamPlayer")
 
-var levelone_one = preload("res://src/environment/LevelMap/L1-1/L1-1_main.tscn")
-var levelone_two = preload("res://src/environment/LevelMap/L1-2/L1-2_main.tscn")
-var levelone_three = preload("res://src/environment/LevelMap/L1-3/L1-3_main.tscn")
-var levelone_four = preload("res://src/environment/LevelMap/L1-3/L1-3_main.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	background_music.playing = true
@@ -17,25 +12,53 @@ func _process(delta):
 		background_music.playing = true
 
 func _on_PlayButton_pressed():
+	var levels = [
+		{ 
+			"name": "Level 1",
+			"src": "res://src/environment/LevelMap/L1-1/L1-1_main.tscn", 
+			# "on_hover": Could have description / picture pop up 
+		},
+		{ 
+			"name": "Level 2", 
+			"src": "res://src/environment/LevelMap/L1-2/L1-2_main.tscn", 
+		},
+		{ 
+			"name": "Level 3", 
+			"src": "res://src/environment/LevelMap/L1-3/L1-3_main.tscn", 
+		},
+		{ 
+			"name": "Level 4", 
+			"src": "res://src/environment/LevelMap/L1-4/L1-4_main.tscn", 
+		},
+	]
+	$LevelSelection.set_up(levels)
+	$LevelSelection.popup_centered()
+	return
+	
+func _on_Tutorial_pressed():
+	var tutorials = [
+		# tutorial 1 to show typical TD
+		# tutorial 2 to drag and drop items
+		{ 
+			"name": "Tutorial 1", # All together
+			"src": "res://src/environment/LevelMap/Tutorial/Tutorial.tscn", 
+			# "on_hover": Could have description / picture pop up 
+		},
+	]
+	$LevelSelection.set_up(tutorials)
+	$LevelSelection.popup_centered()
+	return
+
+# Called by the pop up menu
+func start_level(level_src: String):
 	var arena = load("res://src/ui/gameplay/Arena/Arena.tscn").instance()
-	var basic = load("res://src/environment/LevelMap/L1-1/L1-1_main.tscn")
-	# var basic = load("res://src/environment/LevelMap/BasicLevel.tscn")
+	var level = load(level_src)
 	var root = get_tree().get_root()
 	root.add_child(arena)
-	arena.set_up(levelone_three)
+	arena.set_up(level)
 	
 	root.remove_child(self)
 	self.queue_free()
 
 func _on_Quit_pressed():
 	get_tree().quit()
-	
-func _on_Tutorial_pressed():
-	var arena = load("res://src/ui/gameplay/Arena/Arena.tscn").instance()
-	var basic = load("res://src/environment/LevelMap/Tutorial/Tutorial.tscn")
-	var root = get_tree().get_root()
-	root.add_child(arena)
-	arena.set_up(basic)
-	
-	root.remove_child(self)
-	self.queue_free()
