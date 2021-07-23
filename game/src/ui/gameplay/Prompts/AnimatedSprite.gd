abdb0312d19aa356c5931ced57608d1f7040016b
Github <noreply@github.com>
2021-07-23 04:02:13 +0000

    Let the code elf take a look.

--- game/src/ui/gameplay/Prompts/AnimatedSprite.gd
+++ game/src/ui/gameplay/Prompts/AnimatedSprite.gd
@@ -16,6 +16,8 @@ onready var title = self.get_node("AllText/Title")
 onready var text = self.get_node("AllText/Text")
 onready var arena = self.find_parent("Arena")
 onready var ui = arena.get_node("UI")
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Used to pause the game
@@ -31,9 +33,10 @@ func _on_AnimatedSprite_animation_finished():
 		ui.set_pause_scene(self, false)
 		self.play("Temp")
 
+
 func _input(event: InputEvent):
 	if event is InputEventKey:
-		if event.pressed and !event.is_action_pressed("Settings"):
+		if event.pressed and ! event.is_action_pressed("Settings"):
 			if counter > textArray.size() - 1:
 				ui.set_pause_scene(arena, false)
 				self.queue_free()
