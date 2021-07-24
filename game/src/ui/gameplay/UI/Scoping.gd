e7a973e79b96a1f2405fb54921afd837782617e3
Github <noreply@github.com>
2021-07-24 15:02:12 +0000

    Let the code elf take a look.

--- game/src/ui/gameplay/UI/Scoping.gd
+++ game/src/ui/gameplay/UI/Scoping.gd
@@ -1,17 +1,18 @@
 extends Node2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var counter = 0
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass  # Replace with function body.
+
 
 func _prev():
-	if (counter == 0):
+	if counter == 0:
 		pass
 	else:
 		var child = self.get_child(counter)
@@ -20,8 +21,9 @@ func _prev():
 		prev_child.visible = true
 		counter -= 1
 
+
 func next():
-	if (counter < self.get_child_count() -1):
+	if counter < self.get_child_count() - 1:
 		var child = self.get_child(counter)
 		var next_child = self.get_child(counter + 1)
 		child.visible = false
@@ -29,10 +31,15 @@ func next():
 		counter += 1
 	else:
 		self.queue_free()
-		
+
+
 func _input(event: InputEvent):
 	if event is InputEventKey:
 		if event.pressed and event.is_action_pressed("back_key"):
 			_prev()
-		elif event.pressed and ! event.is_action_pressed("Settings") and ! event.is_action_pressed("back_key"):
+		elif (
+			event.pressed
+			and ! event.is_action_pressed("Settings")
+			and ! event.is_action_pressed("back_key")
+		):
 			next()
